78901bc09e3ed698fdb1c26e0f32cfd477014557
Salman Akram <thunder9ruby@gmail.com>
2015-04-07 18:21:46 -0400

    add user pages via controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+  helper_method :user_signed_in?
+  helper_method :correct_user?
+
   private
   	def current_user
   		begin
   			@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  		rescue Exception => e
+  		rescue exception => e
   			nil
   		end
   	end
@@ -28,15 +32,6 @@ class ApplicationController < ActionController::Base
   			redirect_to root_url, :alert => 'You need to sign in for access to this page.'
   		end
   	end
-
-  end
-
-
-
-
-
-
-
-
-
 end
+
+
